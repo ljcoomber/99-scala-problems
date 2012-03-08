e2912b825b35d054d35c147995eac3208472f972
Lee Coomber <lee@lshift.net>
2012-03-08 18:02:01 +0000

    Add basic tests for P01

--- src/main/scala/org/coomber/lee/scala99/p01.scala
+++ src/main/scala/org/coomber/lee/scala99/p01.scala
@@ -6,13 +6,9 @@ package org.coomber.lee.scala99
  *
  *   scala> last(List(1, 1, 2, 3, 5, 8))
  *   res0: Int = 8
- *
- * TODO: Use reflection (or Scala dynamic dispatch? to test)
  */
 
 object P01 {
-
-  val testList = List(1, 1, 2, 3, 5, 8)
 
   def lastListMethod(l: List[_]) = l.last
 
@@ -22,13 +18,6 @@ object P01 {
     case h :: Nil  => h
     case _ :: tail => lastRecursive(tail)
     case _         => throw new NoSuchElementException
-  }
-
-  // TODO: Break into test class
-  def test() {
-    assert(lastListMethod(testList) == 8)
-    assert(lastListLength(testList) == 8)
-    assert(lastRecursive(testList) == 8)
   }
 }
 
--- src/test/scala/org/coomber/lee/scala99/P01Spec.scala
+++ src/test/scala/org/coomber/lee/scala99/P01Spec.scala
@@ -4,5 +4,18 @@ import org.specs._
 
 object P01Spec extends Specification {
   "P01Spec" should {
+    val testList = List(1, 1, 2, 3, 5, 8)
+
+    "work using method" in {
+      P01.lastListMethod(testList) mustEqual 8
+    }
+
+    "work using length" in {
+      P01.lastListLength(testList) mustEqual 8
+    }
+
+    "work using recursion" in {
+      P01.lastRecursive(testList) mustEqual 8
+    }
   }
 }
