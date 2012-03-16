fd95ec5e614630b11e01a833c150e19c41fc94b2
Lee Coomber <lee@lshift.net>
2012-03-16 17:23:16 +0000

    WiP

--- src/test/scala/org/coomber/lee/scala99/P01Spec.scala
+++ src/test/scala/org/coomber/lee/scala99/P01Spec.scala
@@ -1,21 +1,40 @@
 package org.coomber.lee.scala99
 
-import org.specs._
+import org.specs2.mutable._
+import org.specs2.matcher.DataTables
 
-object P01Spec extends Specification {
+object P01Spec extends Specification /* with DataTables */ {
   "P01Spec" should {
     val testList = List(1, 1, 2, 3, 5, 8)
 
-    "work using method" in {
-      P01.lastListMethod(testList) mustEqual 8
+    val methods = Seq(P01.lastListMethod _,  P01.lastListLength _, P01.lastRecursive _)
+
+    "foo" in {
+      2 mustEqual 2
     }
 
-    "work using length" in {
-      P01.lastListLength(testList) mustEqual 8
+    methods.foreach(println)
+
+
+    "return the last element" in {
+      forall(methods) ((_: (List[Any]))(testList) mustEqual 8)
+      (P01.lastListLength _)(testList) mustEqual 8
     }
 
-    "work using recursion" in {
-      P01.lastRecursive(testList) mustEqual 8
+
+    // TODO: Work out how to do single columns
+    /*
+    "impl" in {
+      "foo" ! P01.lastListMethod _ |>
+      "bar" ! P01.lastListLength _ |
+      "baz" ! P01.lastRecursive _ | {
+        // TODO: Why do I need to supply parameter types?
+        (name: String, impl: (List[_]) => Any ) =>
+          println("Impl: " + impl)
+          impl(testList) mustEqual 7 // TODO: SHOULD FAIL
+      }
     }
+    */
+
   }
 }
