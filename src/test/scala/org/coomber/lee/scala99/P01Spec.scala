6e9afdfdd91182b6608123003ff3e4ffc740fbc4
Lee Coomber <lee@lshift.net>
2012-03-18 17:35:51 +0000

    Fix test, and remove old data table code

--- src/test/scala/org/coomber/lee/scala99/P01Spec.scala
+++ src/test/scala/org/coomber/lee/scala99/P01Spec.scala
@@ -1,40 +1,18 @@
 package org.coomber.lee.scala99
 
 import org.specs2.mutable._
-import org.specs2.matcher.DataTables
 
-object P01Spec extends Specification /* with DataTables */ {
+object P01Spec extends Specification {
   "P01Spec" should {
-    val testList = List(1, 1, 2, 3, 5, 8)
+    val testVals = List(1, 1, 2, 3, 5, 8)
 
-    val methods = Seq(P01.lastListMethod _,  P01.lastListLength _, P01.lastRecursive _)
-
-    "foo" in {
-      2 mustEqual 2
-    }
-
-    methods.foreach(println)
-
+    // TODO: Is there a way of declaring the type more elegantly
+    val methods = Seq[List[Any] => Any](P01.lastListMethod _, 
+                                        P01.lastListLength _,
+                                        P01.lastRecursive _)
 
     "return the last element" in {
-      forall(methods) ((_: (List[Any]))(testList) mustEqual 8)
-      (P01.lastListLength _)(testList) mustEqual 8
+      ((_:List[Any] => Any)(testVals) mustEqual 8).forall(methods) 
     }
-
-
-    // TODO: Work out how to do single columns
-    /*
-    "impl" in {
-      "foo" ! P01.lastListMethod _ |>
-      "bar" ! P01.lastListLength _ |
-      "baz" ! P01.lastRecursive _ | {
-        // TODO: Why do I need to supply parameter types?
-        (name: String, impl: (List[_]) => Any ) =>
-          println("Impl: " + impl)
-          impl(testList) mustEqual 7 // TODO: SHOULD FAIL
-      }
-    }
-    */
-
   }
 }
